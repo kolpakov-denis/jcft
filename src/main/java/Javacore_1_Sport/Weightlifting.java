e1ae19e503e5054f822072ced928636507b2bdd2
denis <veinzettel@ya.ru>
2022-05-19 21:00:54 +0500

    Lesson 1

---
+++ src/main/java/Javacore_1_Sport/Weightlifting.java
@@ -0,0 +1,16 @@
+package Javacore_1_Sport;
+
+public class Weightlifting {
+
+    private int weight;
+
+    public Weightlifting(int weight) {
+        this.weight = weight;
+    }
+
+        public int getWeight() {
+            return weight;
+        }
+
+    }
+
